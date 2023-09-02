c074698472e9c81e3a12ca8355b83278a19d478c
imanegis <143059694+imanegis@users.noreply.github.com>
2023-09-02 20:37:03 +0100

    Add files via upload

---
+++ sample-geojson.js
@@ -0,0 +1,88 @@
+var radarDetails = {
+    "type": "FeatureCollection",
+    "features": [{
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -7.57734989, 33.53870642, 169.22
+            ]
+        },
+        "type": "Feature",
+        "properties": {
+            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+        },
+        "id": 51
+    }, {
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -7.58738366, 33.54271929, 127.44
+            ]
+        },
+        "type": "Feature",
+        "properties": {
+            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+        },
+        "id": 52
+    }, {
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -7.56384471, 33.55397704, 141.67
+            ]
+        },
+        "type": "Feature",
+        "properties": {
+            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+        },
+        "id": 53
+    }, {
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -7.53277664, 33.56328459, 148.68
+            ]
+        },
+        "type": "Feature",
+        "properties": {
+            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+        },
+        "id": 54
+    }, {
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -7.52588983, 33.57121472, 145.5
+            ]
+        },
+        "type": "Feature",
+        "properties": {
+            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+        },
+        "id": 55
+    }, {
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -7.53397413, 33.5733531, 140.11
+            ]
+        },
+        "type": "Feature",
+        "properties": {
+            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+        },
+        "id": 56
+    }, {
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -7.51129446, 33.61270996, 70.97
+            ]
+        },
+        "type": "Feature",
+        "properties": {
+            "popupContent": "Ben M'sik Boulevard Mohammed VI  Ben M'sik-sidi otmane   60  Active  le  meme sens"
+        },
+        "id": 57
+    }]
+};
